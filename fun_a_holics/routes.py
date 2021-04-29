833fdf46a31f78d7bcef34e54cac89dd49a07e60
Akshay <agunda1@asu.edu>
2021-04-29 15:21:48 -0700

    save picture

--- fun_a_holics/routes.py
+++ fun_a_holics/routes.py
@@ -95,15 +95,19 @@ def logout():
     return redirect(url_for('home'))
 
 def save_picture(form_picture):
-    random_hex = secrets.token_hex(8)
-    _, f_ext = os.path.splitext(form_picture.filename)
-    picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
-    output_size = (125, 125)
-    i = Image.open(form_picture)
-    i.thumbnail(output_size)
-    i.save(picture_path)
-    return picture_fn
+    try:
+        random_hex = secrets.token_hex(8)
+        _, f_ext = os.path.splitext(form_picture.filename)
+        picture_fn = random_hex + f_ext
+        picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+        output_size = (125, 125)
+        i = Image.open(form_picture)
+        i.thumbnail(output_size)
+        i.save(picture_path)
+        return picture_fn
+    except Exception as e:
+        print(str(e))
+
 
 def validate_username(username):
     user = select_user_username(username)
